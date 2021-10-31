84a085faa414545058bbfabfb21a706b9289f958
socraticDev <socraticdev@gmail.com>
2021-10-31 15:08:23 -0400

    adding bulgaria and turkey time zones

--- js/beattime.js
+++ js/beattime.js
@@ -3,7 +3,7 @@ function printTime(dateObj) {
   let minutes = dateObj.getMinutes();
 
   let printedHours = hours >= 10 ? hours : "0" + hours;
-  let printedMinutes = minutes >= 10 ? minutes : "0" +minutes;
+  let printedMinutes = minutes >= 10 ? minutes : "0" + minutes;
 
   return printedHours + ":" + printedMinutes;
 }
@@ -51,18 +51,24 @@ const paristimeCell = document.getElementById("paris-time");
 const chiTimeCell = document.getElementById("chicago-time");
 const sftimeCell = document.getElementById("pacific-time");
 const beatimeCell = document.getElementById("beat-time");
+const bulgariaTimeCell = document.getElementById("bulgaria-time");
+const turkeyTimeCell = document.getElementById("turkey-time");
 
 function load() {
   let ny = getDate(luxon.DateTime.now().setZone('America/New_York'), nyOffset);
   let paris = getDate(luxon.DateTime.now().setZone('Europe/Paris'), parisOffset);
   let sf = getDate(luxon.DateTime.now().setZone('America/Los_Angeles'), sfOffset);
   let chicago = getDate(luxon.DateTime.now().setZone('America/Chicago'), chiOffset);
+  let bulgaria = getDate(luxon.DateTime.now().setZone('Europe/Athens'), bulgOffset);
+  let turkey = getDate(luxon.DateTime.now().setZone('Europe/Istanbul'), turkOffset);
 
   beatimeCell.innerText = getBeats(new Date());
   sftimeCell.innerText = printTime(sf);
   nytimeCell.innerText = printTime(ny);
   paristimeCell.innerText = printTime(paris);
   chiTimeCell.innerText = printTime(chicago);
+  bulgariaTimeCell.innerText = printTime(bulgaria);
+  turkeyTimeCell.innerText = printTime(turkey);
 }
 
 load();
